11e8cd6229e595a60f0ed1192f88a5ac21f0ada1
antonioGabrielGomes <antonioggca000@gmail.com>
2020-04-01 15:45:29 -0300

    install frontend

---
+++ frontend/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './pages/home'
+
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/home" exact component={Home} />
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
