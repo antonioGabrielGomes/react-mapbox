f27770d76a26a42e8d1b78146e4b77f1489f28c0
antonioGabrielGomes <antonioggca000@gmail.com>
2020-04-04 02:02:19 -0300

    sáb 04 abr 2020 2h am

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl';
+import Tooltip from './components/tooltip'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import token from './utils/mapbox-token.json'
+
+mapboxgl.accessToken = token.accessToken;
+
+class Application extends React.Component {
+  constructor(props) {
+  super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+
+    map.on('move', () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div className='sidebarStyle'>
+          <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+        </div>
+
+        <div ref={el => this.mapContainer = el} className='mapContainer' />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
